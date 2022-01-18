f976fc7c2f6dff832d1b77c72a33811a16553621
Heon2 <adlgjs@naver.com>
2022-01-18 15:34:04 +0900

    DFS/BFS

---
+++ binary search/이진 탐색.py
@@ -0,0 +1,24 @@
+def binary_search(array, target, start, end):
+    if(start > end):
+        return None
+    mid = (start+end)//2
+    if(array[mid] == target):
+        return mid
+    elif(array[mid] > target):
+        end = mid - 1
+        return binary_search(array, target, start, end)
+    else:
+        start = mid + 1
+        return binary_search(array, target, start, end)
+
+
+# n(원소의 개수) target(찾고자 하는 값)
+n, target = list(map(int, input().split()))
+# 전체 원소 입력 받기
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n-1)
+if(result == None):
+    print("원소가 존재하지 않습니다")
+else:
+    print(result+1)
